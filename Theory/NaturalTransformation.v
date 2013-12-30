087a44925e41ec381ff855e3b55cf599051b117c
rs- <sev3n.rs@gmail.com>
2013-12-30 18:15:56 +0100

    Natural Transformation definition

---
+++ Theory/NaturalTransformation.v
@@ -0,0 +1,16 @@
+Require Import Theory.Category.
+Require Import Theory.Functor.
+
+(*------------------------------------------------------------------------------
+  -- ＮＡＴＵＲＡＬ  ＴＲＡＮＳＦＯＲＭＡＴＩＯＮ  ＤＥＦＩＮＩＴＩＯＮ
+  ----------------------------------------------------------------------------*)
+
+Structure NaturalTransformation {𝒞 𝒟 : Category} (F G : Functor 𝒞 𝒟) := mkNaturalTransformation
+{ η : ∀ A, F A ⇒ G A
+; η_commutes : ∀ {A B} {f : A ⇒ B}, η(B) ∘ F ⋅ f ≈ G ⋅ f ∘ η(A) }.
+
+Arguments mkNaturalTransformation {_ _ _ _ _} _.
+Arguments η                       {_ _ _ _} _ _.
+Arguments η_commutes              {_ _ _ _} _ {_ _ _}.
+
+Notation make η := (@mkNaturalTransformation _ _ _ _ η _) (only parsing).
